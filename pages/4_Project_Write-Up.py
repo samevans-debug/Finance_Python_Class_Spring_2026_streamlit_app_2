01ae3567c644d40134cf5cad98c4bad63be422b9
samevans-debug <samevans@gwmail.gwu.edu>
2026-05-02 18:48:48 -0400

    added a write-up page and a fun photo of george I made in canva..

---
+++ pages/4_Project_Write-Up.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.title("Project Write-Up")
+st.subheader("_Created by Samuel Evans -- Spring 2026_")
+with st.container(border=True):
+    st.markdown("I will add to this page later....")
+with st.container(border=True):
+    st.image("party_George_logo_for_website_fin.png", use_container_width=True)
